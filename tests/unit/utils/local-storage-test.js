2e61940e0361a58ce90475439128b31480707acf
Miguel Madero <mmadero@practicefusion.com>
2016-02-27 21:49:28 -0800

    ES6ifies

--- tests/unit/utils/local-storage-test.js
+++ tests/unit/utils/local-storage-test.js
@@ -1,10 +1,18 @@
-import storage from 'ember-view-state/utils/local-storage';
+import storage, {createInMemoryStorage} from 'ember-view-state/utils/local-storage';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | local storage');
 
-// Replace this with your real tests.
 test('storage contains a function to getItem and setItem even in private browsing in Safari', function(assert) {
   assert.ok(storage.getItem);
   assert.ok(storage.setItem);
+});
+
+test('createInMemoryStorage', function(assert) {
+  const storage = createInMemoryStorage();
+  assert.ok(storage.getItem);
+  assert.ok(storage.setItem);
+
+  storage.setItem('test', 1);
+  assert.equal(storage.getItem('test'), 1);
 });
